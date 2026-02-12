bb856361aa8c92289e971206a646864eaf7c24f8
maxxijewsa <gnsnoob76@gmail.com>
2026-02-13 04:13:29 +0530

    Update static-data.ts

--- client/src/data/static-data.ts
+++ client/src/data/static-data.ts
@@ -1,10 +1,10 @@
 import type { Stats, Testimonial } from "@shared/schema";
 
 export const stats: Stats = {
-  totalServers: "152+",
+  totalServers: "174+",
   totalUsers: "34645+",
   uptime: "99.9%",
-  threatsblocked: "205+",
+  threatsblocked: "361+",
 };
 
 export const testimonials: Testimonial[] = [
